b69fc79aba7d91b18906d5b946624de5bc444263
rafa14-maker <rafayeatak47@gmail.com>
2023-11-11 12:56:33 +0600

    started learning

---
+++ Lesson-2 Basics/index.py
@@ -0,0 +1,16 @@
+namefirst = 'fahim'
+print(2+2)
+print("first")
+
+
+line01 = "***************" #header / footer
+line02 = "*             *" # re-use
+line03 = "*   Welcome!  *" 
+
+#starts with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
---
+++ Lesson-3 Oparators/index.py
@@ -0,0 +1,11 @@
+print('Hello')
+print(24 % 5)
+print("Name" + " Rafayet")
+
+meaning = 42
+
+
+if meaning > 10:
+    print('Right on!')
+else: 
+    print('not today') 
