3d2c1520be106f5db7f1d95096f11efd8025496f
rafa14-maker <rafayeatak47@gmail.com>
2023-11-11 18:36:46 +0600

    learned about datatype and oparators

--- Lesson-3 Oparators/index.py
+++ Lesson-3 Oparators/index.py
@@ -3,9 +3,10 @@ print(24 % 5)
 print("Name" + " Rafayet")
 
 meaning = 42
-
-
+          
 if meaning > 10:
     print('Right on!')
 else: 
-    print('not today') 
+    print('not today')
+
+print('Right On!') if meaning > 10 else print ('Not today') 
---
+++ Lesson-4 DataTypes/index.py
@@ -0,0 +1,143 @@
+import math
+
+print('hello')
+
+def hello(n):
+    return n*n 
+
+
+print(hello(9))
+
+#String data type 
+
+# literal assignment
+first = "Dave"
+last = "Gray"
+
+# print(type(first) == str)
+# print(type(first))
+# print(isinstance(first,str))
+
+#constructor function
+# pizza = str("peparonni")
+# print(type(pizza) == str)
+# print(type(pizza))
+# print(isinstance(pizza,str))
+
+# concatination 
+fullname = first + " " + last 
+print(fullname)
+fullname += "!"
+print(fullname)
+
+#casting a number to string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "i like rock music from the " + decade + "s."
+print(statement)
+
+#multiple lines
+multiline = ''' 
+hey, how are you ?
+
+i was just checking in .
+
+         all good ?
+'''
+print(multiline)
+
+
+# Escaping special characters 
+sentence = 'I\'m back at work!\they!\n\nWhers\s this at\\located ?'
+print(sentence)
+
+#string methods 
+
+print(first)
+print(first.lower())
+print(first.upper())
+print(first)
+
+print(multiline.title())
+print(multiline.replace("good","ok"))
+print(multiline)
+
+print(len(multiline))
+multiline += "                             "
+multiline  = "                 " + multiline
+print(len(multiline))
+
+
+print(len(multiline.strip()))
+print(len(multiline.lstrip()))
+print(len(multiline.rstrip()))
+
+
+
+#build a menu 
+title = 'menu'.upper()
+
+print(title.center(20,'='))
+print("Coffee".ljust(16,".") + "$1".rjust(4))
+print("Muffin".ljust(16,".") + "$2".rjust(4))
+print("ChesseCake".ljust(16,".") + "$6".rjust(4))
+
+print("")
+
+#string index values
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+#Some methods return boolean data
+print(first.startswith("D"))
+print(first.endswith("Z"))
+
+
+# Boolean Data type 
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue,bool))
+
+#numeric data type 
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price,int))
+
+
+# float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+#complex type 
+# comp_value = 5+3j
+
+# print(type(comp_value))
+# print(comp_value.img)
+# print(comp_value.real)
+
+#bulit in functions for numbers 
+
+print(abs(gpa))
+print(abs(gpa * -1))
+print(round(gpa))
+print(round(gpa,1))
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a String to a number 
+zipcode = "10001"
+zip_value = int(zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data 
+# zip_value int("New York")
